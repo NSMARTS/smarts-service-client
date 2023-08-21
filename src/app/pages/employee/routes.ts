ce135a27d8666df7f82d6711481e571ed7f030b1
nsmarts-jmh <mcho@nsmarts.co.kr>
2023-08-21 09:13:33 +0900

    왜 인지 모르지만 employee 컴포넌트 일부가 없음

--- src/app/pages/employee/routes.ts
+++ src/app/pages/employee/routes.ts
@@ -1,24 +1,22 @@
 import { Route } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { EmployeeAddComponent } from './employee-add/employee-add.component';
-import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
-import { EmployeeViewComponent } from './employee-view/employee-view.component';
+
 import { EmployeeCompanyListComponent } from './company-list/employee-company-list.component';
 
 export const EMPLOYEE_ROUTES: Route[] = [
   {
     path: '',
     component: EmployeeListComponent,
-    children: [
-      {
-        path: 'edit/:id',
-        component: EmployeeEditComponent,
-      },
-      {
-        path: 'view/:id',
-        component: EmployeeViewComponent,
-      },
-    ],
+    // children: [
+    //   {
+    //     path: 'edit/:id',
+    //     component: EmployeeEditComponent,
+    //   },
+    //   {
+    //     path: 'view/:id',
+    //     component: EmployeeViewComponent,
+    //   },
+    // ],
   },
   {
     path: 'company-list',
@@ -26,6 +24,6 @@ export const EMPLOYEE_ROUTES: Route[] = [
   },
   {
     path: 'add',
-    component: EmployeeAddComponent,
+    // component: EmployeeAddComponent,
   },
 ];
