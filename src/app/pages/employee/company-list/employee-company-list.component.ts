5e647691d039273ec43b0d08c11a4cb8653cd932
ljwoon <ljwoon@nsmarts.co.kr>
2023-08-21 14:27:50 +0900

    upadate

--- src/app/pages/employee/company-list/employee-company-list.component.ts
+++ src/app/pages/employee/company-list/employee-company-list.component.ts
@@ -10,99 +10,100 @@ import { Company } from 'src/app/interfaces/company.interface';
 import { HttpResMsg } from 'src/app/interfaces/http-response.interfac';
 
 @Component({
-  selector: 'app-employee-company-list',
-  standalone: true,
-  imports: [CommonModule, MaterialsModule, RouterModule],
-  //   standalone: true,
-  templateUrl: './employee-company-list.component.html',
-  styleUrls: ['./employee-company-list.component.scss'],
+    selector: 'app-employee-company-list',
+    standalone: true,
+    imports: [CommonModule, MaterialsModule, RouterModule],
+    //   standalone: true,
+    templateUrl: './employee-company-list.component.html',
+    styleUrls: ['./employee-company-list.component.scss'],
 })
 export class EmployeeCompanyListComponent {
-  displayedColumns: string[] = [
-    'code',
-    'name',
-    'employees',
-    'rollover',
-    // 'rolloverMaxMonth',
-    // 'rolloverMaxDay',
-    // 'btns',
-  ];
-  filterValues: any = {};
-  filterSelectObj: any = [];
-  company_max_day: any;
-  isRollover = false;
+    displayedColumns: string[] = [
+        'code',
+        'name',
+        'employees',
+        'annual_policy',
+        'rollover',
+        'replacementDay',
+        'minusAnnualLeave'
 
-  dataSource: MatTableDataSource<Company> = new MatTableDataSource<Company>([]);
-  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+    ];
+    filterValues: any = {};
+    filterSelectObj: any = [];
+    company_max_day: any;
+    isRollover = false;
 
-  constructor(
-    private router: Router,
-    private companyService: CompanyService // public dialogService: DialogService
-  ) {}
+    dataSource: MatTableDataSource<Company> = new MatTableDataSource<Company>([]);
+    @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
 
-  ngOnInit(): void {
-    this.getCompanyList();
-  }
+    constructor(
+        private router: Router,
+        private companyService: CompanyService // public dialogService: DialogService
+    ) { }
 
-  /**
-   * 컴퍼니 리스트 호출
-   */
-  getCompanyList() {
-    this.companyService.getCompanyList().subscribe({
-      next: (res: HttpResMsg<Company[]>) => {
-        const company = res.data;
-        console.log(company);
-        this.dataSource = new MatTableDataSource(company);
-        this.dataSource.paginator = this.paginator;
-      },
-      error: (err) => console.error(err),
-    });
-  }
-
-  backManagerList() {
-    this.router.navigate(['employee']);
-  }
-
-  // 회사 수정
-  editCompany(id: any) {
-    this.router.navigate(['company/company-edit/' + id]);
-  }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
+    ngOnInit(): void {
+        this.getCompanyList();
     }
-  }
 
-  createEmployee() {
-    this.router.navigate(['employee/company-list']);
-  }
-  // 회사 삭제
-  //   deleteCompany(id: any) {
-  //     this.dialogService
-  //       .openDialogConfirm('Do you delete this company?')
-  //       .subscribe((result: any) => {
-  //         if (result) {
-  //           // 회사 삭제
-  //           this.companyService.deleteCompany({ _id: id }).subscribe(
-  //             (data: any) => {
-  //               if (data.message == 'delete company') {
-  //                 this.dialogService.openDialogPositive(
-  //                   'Successfully, the company has been delete.'
-  //                 );
-  //                 this.getCompanyList();
-  //               }
-  //             },
-  //             (err: any) => {
-  //               console.log(err);
-  //               this.dialogService.openDialogNegative(err.error.message);
-  //               // alert(err.error.message);
-  //             }
-  //           );
-  //         }
-  //       });
-  //   }
+    /**
+     * 컴퍼니 리스트 호출
+     */
+    getCompanyList() {
+        this.companyService.getCompanyList().subscribe({
+            next: (res: HttpResMsg<Company[]>) => {
+                const company = res.data;
+                console.log(company);
+                this.dataSource = new MatTableDataSource(company);
+                this.dataSource.paginator = this.paginator;
+            },
+            error: (err) => console.error(err),
+        });
+    }
+
+    backManagerList() {
+        this.router.navigate(['employee']);
+    }
+
+    // 회사 수정
+    editCompany(id: any) {
+        this.router.navigate(['company/company-edit/' + id]);
+    }
+
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+
+        if (this.dataSource.paginator) {
+            this.dataSource.paginator.firstPage();
+        }
+    }
+
+    createEmployee() {
+        this.router.navigate(['employee/company-list']);
+    }
+    // 회사 삭제
+    //   deleteCompany(id: any) {
+    //     this.dialogService
+    //       .openDialogConfirm('Do you delete this company?')
+    //       .subscribe((result: any) => {
+    //         if (result) {
+    //           // 회사 삭제
+    //           this.companyService.deleteCompany({ _id: id }).subscribe(
+    //             (data: any) => {
+    //               if (data.message == 'delete company') {
+    //                 this.dialogService.openDialogPositive(
+    //                   'Successfully, the company has been delete.'
+    //                 );
+    //                 this.getCompanyList();
+    //               }
+    //             },
+    //             (err: any) => {
+    //               console.log(err);
+    //               this.dialogService.openDialogNegative(err.error.message);
+    //               // alert(err.error.message);
+    //             }
+    //           );
+    //         }
+    //       });
+    //   }
 }
--- src/app/pages/employee/routes.ts
+++ src/app/pages/employee/routes.ts
@@ -4,26 +4,26 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeCompanyListComponent } from './company-list/employee-company-list.component';
 
 export const EMPLOYEE_ROUTES: Route[] = [
-  {
-    path: '',
-    component: EmployeeListComponent,
-    // children: [
-    //   {
-    //     path: 'edit/:id',
-    //     component: EmployeeEditComponent,
-    //   },
-    //   {
-    //     path: 'view/:id',
-    //     component: EmployeeViewComponent,
-    //   },
-    // ],
-  },
-  {
-    path: 'company-list',
-    component: EmployeeCompanyListComponent,
-  },
-  {
-    path: 'add',
+    {
+        path: '',
+        component: EmployeeListComponent,
+        // children: [
+        //   {
+        //     path: 'edit/:id',
+        //     component: EmployeeEditComponent,
+        //   },
+        //   {
+        //     path: 'view/:id',
+        //     component: EmployeeViewComponent,
+        //   },
+        // ],
+    },
+    {
+        path: 'company-list',
+        component: EmployeeCompanyListComponent,
+    },
+    // {
+    // path: 'add',
     // component: EmployeeAddComponent,
-  },
+    // },
 ];
