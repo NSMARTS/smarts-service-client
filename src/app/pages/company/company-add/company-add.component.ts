d1cf3e6b2e88940d51f677c3079dabaef2515056
ljwoon <ljwoon@nsmarts.co.kr>
2023-08-21 13:12:15 +0900

    update

--- src/app/pages/company/company-add/company-add.component.ts
+++ src/app/pages/company/company-add/company-add.component.ts
@@ -4,11 +4,11 @@ import { Router, RouterModule } from '@angular/router';
 import { Subject } from 'rxjs';
 
 import {
-  FormArray,
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
+    FormArray,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
 } from '@angular/forms';
 import { DataService } from 'src/app/stores/data/data.service';
 import { CommonModule } from '@angular/common';
@@ -16,114 +16,126 @@ import { MaterialsModule } from 'src/app/materials/materials.module';
 import { CompanyService } from 'src/app/services/company.service';
 
 @Component({
-  selector: 'app-company-add',
-  standalone: true,
-  imports: [CommonModule, MaterialsModule, RouterModule, ReactiveFormsModule],
-  templateUrl: './company-add.component.html',
-  styleUrls: ['./company-add.component.scss'],
+    selector: 'app-company-add',
+    standalone: true,
+    imports: [CommonModule, MaterialsModule, RouterModule, ReactiveFormsModule],
+    templateUrl: './company-add.component.html',
+    styleUrls: ['./company-add.component.scss'],
 })
 export class CompanyAddComponent {
-  addCompanyForm: FormGroup;
+    addCompanyForm: FormGroup;
 
-  leaveStandard!: FormArray; // 연차 정책 form
+    leaveStandards!: FormArray; // 연차 정책 form
 
-  leaveStandardYear: number = 0; // 근속년수
+    leaveStandardsYear: number = 0; // 근속년수
 
-  constructor(
-    private router: Router,
-    // private dialogService: DialogService,
-    private formBuilder: FormBuilder,
-    private companyService: CompanyService
-  ) {
-    this.addCompanyForm = this.formBuilder.group({
-      companyName: [''],
-      leaveStandard: this.formBuilder.array([
-        this.createItem(this.leaveStandardYear),
-      ]),
-      isRollover: [false],
-      rolloverMaxMonth: [0],
-      rolloverMaxDay: [0],
-      countryCode: [''],
-      isReplacementDay: [false],
-      rdValidityTerm: [0],
-      isMinusAnnualLeave: [false],
-      annualPolicy: ['byContract'],
-    });
-  }
+    constructor(
+        private router: Router,
+        // private dialogService: DialogService,
+        private formBuilder: FormBuilder,
+        private companyService: CompanyService
+    ) {
+        this.addCompanyForm = this.formBuilder.group({
+            companyName: [''],
+            leaveStandards: this.formBuilder.array([]),
+            isRollover: [false],
+            rolloverMaxMonth: [0],
+            rolloverMaxDay: [0],
+            countryCode: [''],
+            isReplacementDay: [false],
+            rdValidityTerm: [0],
+            isMinusAnnualLeave: [false],
+            annualPolicy: ['byContract'],
+        });
 
-  getControls() {
-    return (this.addCompanyForm.get('leaveStandard') as FormArray).controls;
-  }
-
-  //////////////////////////////////
-  createItem(i: number): FormGroup {
-    return this.formBuilder.group({
-      year: i + 1, // 근속년수
-      annualLeave: '',
-      sickLeave: '',
-    });
-  }
-
-  addItem() {
-    this.leaveStandard = this.addCompanyForm.get('leaveStandard') as FormArray;
-    this.leaveStandard.push(this.createItem(this.leaveStandardYear));
-  }
-
-  cancelItem(i: any) {
-    if (this.leaveStandard) {
-      this.leaveStandard.removeAt(i);
+        this.leaveStandards = this.addCompanyForm.get('leaveStandards') as FormArray;
+        this.addItem();
     }
-  }
 
-  toBack(): void {
-    this.router.navigate(['company']);
-  }
+    getLeaveStandardsControls() {
+        return this.leaveStandards.controls;
+    }
 
-  onSubmit() {
-    // 회사 추가
-    this.addCompany();
-  }
 
-  // 회사 추가
-  addCompany() {
-    const isRollover = this.addCompanyForm.get('isRollover')?.value;
-    const isReplacementDay = this.addCompanyForm.get('isReplacementDay')?.value;
+    //////////////////////////////////
+    createLeaveStandard(year: number): FormGroup {
+        return this.formBuilder.group({
+            year,
+            annualLeave: 0,
+            sickLeave: 0,
+        });
+    }
 
-    const companyData = {
-      ...this.addCompanyForm.value,
-      // 연차날짜를 입력한 후 연차모드를 false로 바꿨을 시 0으로 초기화
-      rolloverMaxMonth: isRollover
-        ? this.addCompanyForm.get('rolloverMaxMonth')?.value
-        : 0,
-      rolloverMaxDay: isRollover
-        ? this.addCompanyForm.get('rolloverMaxDay')?.value
-        : 0,
-      rdValidityTerm: isReplacementDay
-        ? this.addCompanyForm.get('rdValidityTerm')?.value
-        : 0,
-    };
-    //data : this.addCompanyForm.value
+    addItem() {
+        const newYear = this.leaveStandards.length + 1;
+        const newLeaveStandard = this.createLeaveStandard(newYear);
+        this.leaveStandards.push(newLeaveStandard);
+        this.updateYears();
+    }
 
-    this.companyService.addCompany(companyData).subscribe({
-      next: (res) => {
-        if (res) {
-          this.router.navigate(['company']);
+    cancelItem(index: number) {
+        if (this.leaveStandards.length > index) {
+            this.leaveStandards.removeAt(index);
+            this.updateYears();
         }
-      },
-      error: (err) => console.error(err),
-    });
-  }
-
-  errorAlert(err: any) {
-    switch (err) {
-      case 'Duplicate requestLeave':
-        // this.dialogService.openDialogNegative('Duplicate requestLeave.');
-        break;
-      case 'DB Error':
-        // this.dialogService.openDialogNegative(
-        //   'An error has occurred while requesting'
-        // );
-        break;
     }
-  }
+
+    updateYears() {
+        this.leaveStandards.controls.forEach((group, index) => {
+            group.get('year')?.setValue(index + 1);
+        });
+    }
+
+    toBack(): void {
+        this.router.navigate(['company']);
+    }
+
+    onSubmit() {
+        this.addCompany();
+    }
+
+
+    // 회사 추가
+    addCompany() {
+        const isRollover = this.addCompanyForm.get('isRollover')?.value;
+        const isReplacementDay = this.addCompanyForm.get('isReplacementDay')?.value;
+
+        const companyData = {
+            ...this.addCompanyForm.value,
+            // 연차날짜를 입력한 후 연차모드를 false로 바꿨을 시 0으로 초기화
+            rolloverMaxMonth: isRollover
+                ? this.addCompanyForm.get('rolloverMaxMonth')?.value
+                : 0,
+            rolloverMaxDay: isRollover
+                ? this.addCompanyForm.get('rolloverMaxDay')?.value
+                : 0,
+            rdValidityTerm: isReplacementDay
+                ? this.addCompanyForm.get('rdValidityTerm')?.value
+                : 0,
+        };
+        //data : this.addCompanyForm.value
+        console.log(companyData)
+
+        this.companyService.addCompany(companyData).subscribe({
+            next: (res) => {
+                if (res) {
+                    this.router.navigate(['company']);
+                }
+            },
+            error: (err) => console.error(err),
+        });
+    }
+
+    errorAlert(err: any) {
+        switch (err) {
+            case 'Duplicate requestLeave':
+                // this.dialogService.openDialogNegative('Duplicate requestLeave.');
+                break;
+            case 'DB Error':
+                // this.dialogService.openDialogNegative(
+                //   'An error has occurred while requesting'
+                // );
+                break;
+        }
+    }
 }
--- src/app/pages/company/company-edit/company-edit.component.ts
+++ src/app/pages/company/company-edit/company-edit.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import {
-  FormArray,
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
+    FormArray,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
 } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import * as moment from 'moment';
@@ -17,132 +17,132 @@ import { DataService } from 'src/app/stores/data/data.service';
 // import { DialogService } from 'src/@dw/dialog/dialog.service';
 
 @Component({
-  selector: 'app-company-edit',
-  standalone: true,
-  imports: [CommonModule, MaterialsModule, RouterModule, ReactiveFormsModule],
-  templateUrl: './company-edit.component.html',
-  styleUrls: ['./company-edit.component.scss'],
+    selector: 'app-company-edit',
+    standalone: true,
+    imports: [CommonModule, MaterialsModule, RouterModule, ReactiveFormsModule],
+    templateUrl: './company-edit.component.html',
+    styleUrls: ['./company-edit.component.scss'],
 })
 export class CompanyEditComponent implements OnInit {
-  ///////////////////////////////////////
-  editCompanyId: any;
-  ///////////////////////////////////////
+    ///////////////////////////////////////
+    editCompanyId: any;
+    ///////////////////////////////////////
 
-  days: any;
-  start_date_sec: any;
-  end_date_sec: any;
-  millisecondsPerDay: any;
-  diff: any;
-  weeks: any;
-  leaveDays: any;
+    days: any;
+    start_date_sec: any;
+    end_date_sec: any;
+    millisecondsPerDay: any;
+    diff: any;
+    weeks: any;
+    leaveDays: any;
 
-  leaveStandardYear: number = 0; // 근속년수
+    leaveStandardsYear: number = 0; // 근속년수
 
-  leaveStandard!: FormArray;
-  editCompanyForm: FormGroup;
+    leaveStandards!: FormArray;
+    editCompanyForm: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private dataService: DataService,
-    private router: Router,
-    // private dialogService: DialogService,
-    private formBuilder: FormBuilder,
-    private companyService: CompanyService,
-    private route: ActivatedRoute
-  ) {
-    this.editCompanyForm = this.formBuilder.group({
-      companyName: [''],
-      leaveStandard: this.formBuilder.array([
-        this.createItem(this.leaveStandardYear),
-      ]),
-      isRollover: [false],
-      rolloverMaxMonth: [0],
-      rolloverMaxDay: [0],
-      isReplacementDay: [''],
-      isMinusAnnualLeave: [''],
-      rdValidityTerm: [''],
-      annualPolicy: ['byContract'],
-    });
-  }
-
-  ngOnInit(): void {
-    this.editCompanyId = this.route.snapshot.params['id'];
-    this.companyService.getCompanyInfo(this.editCompanyId).subscribe({
-      next: (res) => {
-        const companyData = res.data;
-        this.editCompanyForm.patchValue(companyData);
-        this.leaveStandard = this.editCompanyForm.get(
-          'leaveStandard'
-        ) as FormArray;
-        this.leaveStandard.clear(); // 기존 컨트롤 제거
-        // 새로운 컨트롤 추가
-        console.log(companyData.leaveStandard);
-        for (let i = 0; i < this.leaveStandard.length; i++) {
-          this.leaveStandardYear = i + 1; // 근속년수
-          this.leaveStandard.push(this.createItem(this.leaveStandardYear));
-        }
-      },
-      error: (err) => console.error(err),
-    });
-    /////////////////////////////////////////////////////////////////////////////
-  }
-
-  //////////////////////////////////
-  createItem(i: number): FormGroup {
-    return this.formBuilder.group({
-      year: i,
-      annualLeave: 0,
-      sickLeave: 0,
-    });
-  }
-
-  getControls() {
-    return (this.editCompanyForm.get('leaveStandard') as FormArray).controls;
-  }
-
-  addItem() {
-    this.leaveStandard = this.editCompanyForm.get('leaveStandard') as FormArray;
-    this.leaveStandard.push(this.createItem(this.leaveStandardYear));
-  }
-
-  cancelItem(i: any) {
-    if (this.leaveStandard) {
-      this.leaveStandard.removeAt(i);
+    constructor(
+        private fb: FormBuilder,
+        private dataService: DataService,
+        private router: Router,
+        // private dialogService: DialogService,
+        private formBuilder: FormBuilder,
+        private companyService: CompanyService,
+        private route: ActivatedRoute
+    ) {
+        this.editCompanyForm = this.formBuilder.group({
+            companyName: [''],
+            leaveStandards: this.formBuilder.array([
+                this.createItem(this.leaveStandardsYear),
+            ]),
+            isRollover: [false],
+            rolloverMaxMonth: [0],
+            rolloverMaxDay: [0],
+            isReplacementDay: [''],
+            isMinusAnnualLeave: [''],
+            rdValidityTerm: [''],
+            annualPolicy: ['byContract'],
+        });
     }
-  }
 
-  //////////////////////////////////
-
-  toBack(): void {
-    this.router.navigate(['company']);
-  }
-
-  onSubmit() {
-    // 회사 추가
-    const companyData = {
-      ...this.editCompanyForm.value,
-    };
-
-    this.companyService.editCompany(this.editCompanyId, companyData).subscribe({
-      next: (res) => {
-        if (res) {
-          this.router.navigate(['company']);
-        }
-      },
-      error: (err) => console.error(err),
-    });
-  }
-
-  errorAlert(err: any) {
-    switch (err) {
-      case 'Duplicate requestLeave':
-        // this.dialogService.openDialogNegative('Duplicate requestLeave.');
-        break;
-      case 'DB Error':
-        // this.dialogService.openDialogNegative(
-        //   'An error has occurred while requesting'
-        // );
-        break;
+    ngOnInit(): void {
+        this.editCompanyId = this.route.snapshot.params['id'];
+        this.companyService.getCompanyInfo(this.editCompanyId).subscribe({
+            next: (res) => {
+                const companyData = res.data;
+                this.editCompanyForm.patchValue(companyData);
+                this.leaveStandards = this.editCompanyForm.get(
+                    'leaveStandards'
+                ) as FormArray;
+                this.leaveStandards.clear(); // 기존 컨트롤 제거
+                // 새로운 컨트롤 추가
+                console.log(companyData.leaveStandards);
+                for (let i = 0; i < this.leaveStandards.length; i++) {
+                    this.leaveStandardsYear = i + 1; // 근속년수
+                    this.leaveStandards.push(this.createItem(this.leaveStandardsYear));
+                }
+            },
+            error: (err) => console.error(err),
+        });
+        /////////////////////////////////////////////////////////////////////////////
     }
-  }
+
+    //////////////////////////////////
+    createItem(i: number): FormGroup {
+        return this.formBuilder.group({
+            year: i,
+            annualLeave: 0,
+            sickLeave: 0,
+        });
+    }
+
+    getLeaveStandards() {
+        return (this.editCompanyForm.get('leaveStandards') as FormArray).controls;
+    }
+
+    addItem() {
+        this.leaveStandards = this.editCompanyForm.get('leaveStandards') as FormArray;
+        this.leaveStandards.push(this.createItem(this.leaveStandardsYear));
+    }
+
+    cancelItem(i: any) {
+        if (this.leaveStandards) {
+            this.leaveStandards.removeAt(i);
+        }
+    }
+
+    //////////////////////////////////
+
+    toBack(): void {
+        this.router.navigate(['company']);
+    }
+
+    onSubmit() {
+        // 회사 추가
+        const companyData = {
+            ...this.editCompanyForm.value,
+        };
+
+        this.companyService.editCompany(this.editCompanyId, companyData).subscribe({
+            next: (res) => {
+                if (res) {
+                    this.router.navigate(['company']);
+                }
+            },
+            error: (err) => console.error(err),
+        });
+    }
+
+    errorAlert(err: any) {
+        switch (err) {
+            case 'Duplicate requestLeave':
+                // this.dialogService.openDialogNegative('Duplicate requestLeave.');
+                break;
+            case 'DB Error':
+                // this.dialogService.openDialogNegative(
+                //   'An error has occurred while requesting'
+                // );
+                break;
+        }
+    }
 }
