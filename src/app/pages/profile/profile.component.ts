6ed23f80628a221c06476a59f1a7fe60f2eb849f
nsmarts-jmh <mcho@nsmarts.co.kr>
2023-09-06 13:41:14 +0900

    merge

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -2,19 +2,32 @@ import { AuthService } from './../../services/auth.service';
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialsModule } from 'src/app/materials/materials.module';
-import { DialogService } from 'src/app/dialog/dialog.service';
+
 import { ProfileService } from 'src/app/services/profile.service';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { CountryService } from 'src/app/services/country.service';
 import { Country } from 'src/app/interfaces/employee.interface';
 import { tap } from 'rxjs';
+import { DialogService } from 'src/app/services/dialog.service';
 
-export const comparePasswordValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+export const comparePasswordValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
   const password = control.get('password');
   const confirmPassword = control.get('confirmPassword');
 
-
-  return password && confirmPassword && password.value !== confirmPassword.value ? { isNotMatched: true } : null;
+  return password && confirmPassword && password.value !== confirmPassword.value
+    ? { isNotMatched: true }
+    : null;
 };
 
 @Component({
@@ -22,32 +35,32 @@ export const comparePasswordValidator: ValidatorFn = (control: AbstractControl):
   standalone: true,
   imports: [CommonModule, MaterialsModule, ReactiveFormsModule],
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-
-  authService = inject(AuthService)
-  userInfoStore = this.authService.userInfoStore
+  authService = inject(AuthService);
+  userInfoStore = this.authService.userInfoStore;
   editProfileForm: FormGroup;
-  dialogService = inject(DialogService)
-  countryService = inject(CountryService)
-  profileService = inject(ProfileService)
+  dialogService = inject(DialogService);
+  countryService = inject(CountryService);
+  profileService = inject(ProfileService);
   countryList: Country[] = [];
 
-  constructor(
-    private fb: FormBuilder,
-  ) {
-    this.editProfileForm = this.fb.group({
-      username: new FormControl('', [Validators.required]),
-      country: new FormControl('', [Validators.required]),
-      // empStartDate: new FormControl('', [Validators.required]),
-      // empEndDate: new FormControl(''),
-      // department: new FormControl(''),
-      phoneNumber: new FormControl(''),
-      password: new FormControl(''),
-      confirmPassword: new FormControl(''),
-      profileImgPath: new FormControl(''),
-    }, { validators: comparePasswordValidator });
+  constructor(private fb: FormBuilder) {
+    this.editProfileForm = this.fb.group(
+      {
+        username: new FormControl('', [Validators.required]),
+        country: new FormControl('', [Validators.required]),
+        // empStartDate: new FormControl('', [Validators.required]),
+        // empEndDate: new FormControl(''),
+        // department: new FormControl(''),
+        phoneNumber: new FormControl(''),
+        password: new FormControl(''),
+        confirmPassword: new FormControl(''),
+        profileImgPath: new FormControl(''),
+      },
+      { validators: comparePasswordValidator }
+    );
   }
 
   ngOnInit() {
@@ -71,20 +84,23 @@ export class ProfileComponent implements OnInit {
   //   });
   // }
 
-
   /**
-     * profile 사진 변경
-     * @param fileInput image file
-     */
+   * profile 사진 변경
+   * @param fileInput image file
+   */
   fileChangeEvent(event: any) {
     console.log('fileChangeEvent');
     if (event.target.files && event.target.files[0]) {
-      if (event.target.files[0].name.toLowerCase().endsWith('.jpg')
-        || event.target.files[0].name.toLowerCase().endsWith('.png')) {
+      if (
+        event.target.files[0].name.toLowerCase().endsWith('.jpg') ||
+        event.target.files[0].name.toLowerCase().endsWith('.png')
+      ) {
         // Image resize and update
         this.changeProfileImage(event.target.files[0]);
       } else {
-        this.dialogService.openDialogNegative('Profile photos are only available for PNG and JPG.');
+        this.dialogService.openDialogNegative(
+          'Profile photos are only available for PNG and JPG.'
+        );
         // alert('프로필 사진은 PNG와 JPG만 가능합니다.');
       }
     } else {
@@ -94,53 +110,59 @@ export class ProfileComponent implements OnInit {
   }
 
   changeProfileImage(imgFile: File) {
-    this.profileService.changeProfileImg(imgFile, this.userInfoStore()._id).subscribe({
-      next: (res) => {
-        this.userInfoStore.update((prev) => {
-          return { ...prev, profileImgPath: res.data }
-        })
-      },
-      error: (error) => { console.log(error) }
-    })
-
+    this.profileService
+      .changeProfileImg(imgFile, this.userInfoStore()._id)
+      .subscribe({
+        next: (res) => {
+          this.userInfoStore.update((prev) => {
+            return { ...prev, profileImgPath: res.data };
+          });
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
   }
 
   updateProfile() {
     const patchData = {
       ...this.editProfileForm.value,
-      _id: this.userInfoStore()._id
-    }
+      _id: this.userInfoStore()._id,
+    };
 
     // updateForm 중 값이 ''이면 객체에서 삭제. patch 시 변경될 값만 설정
     for (const key in patchData) {
-      if (patchData.hasOwnProperty(key) && patchData[key] === "") {
+      if (patchData.hasOwnProperty(key) && patchData[key] === '') {
         delete patchData[key];
       }
     }
 
     this.profileService.updateProfile(patchData).subscribe({
       next: (res) => {
-        if (res) this.refreshProfie()
+        if (res) this.refreshProfie();
       },
-      error: (error) => { console.log(error) },
-    })
+      error: (error) => {
+        console.log(error);
+      },
+    });
   }
 
   refreshProfie() {
     this.authService.refreshToken().subscribe({
       next: async (data) => await this.authService.setAccessToken(data),
-      error: (error) => console.log(error)
-    })
+      error: (error) => console.log(error),
+    });
   }
 
   getErrorMessage() {
-    if (this.editProfileForm?.controls['password'].errors?.['required'] || this.editProfileForm?.controls['confirmPassword'].errors?.['required']) {
+    if (
+      this.editProfileForm?.controls['password'].errors?.['required'] ||
+      this.editProfileForm?.controls['confirmPassword'].errors?.['required']
+    ) {
       return 'You must enter a value';
     }
-    return this.editProfileForm?.errors?.['isNotMatched'] ? 'Passwords do not match.' : '';
+    return this.editProfileForm?.errors?.['isNotMatched']
+      ? 'Passwords do not match.'
+      : '';
   }
-
-
-
-
 }
