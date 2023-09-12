67e6eb48fce5aea01d3678c0e1175e40df3a0657
nsmarts-jmh <mcho@nsmarts.co.kr>
2023-09-12 15:07:08 +0900

    [feat] main dashboard allList 제외하고 완료

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,14 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { MaterialsModule } from 'src/app/materials/materials.module';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { DashboardService } from 'src/app/services/dashboard.service';
+
+export interface PeriodicElement {
+  date: string;
+  content: string;
+  company: string;
+}
 
 @Component({
-    selector: 'app-main',
-    standalone: true,
-    imports: [CommonModule, RouterModule],
-    templateUrl: './main.component.html',
-    styleUrls: ['./main.component.scss']
+  selector: 'app-main',
+  standalone: true,
+  imports: [CommonModule, RouterModule, MaterialsModule],
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  displayedColumns: string[] = ['date', 'content', 'company', 'detail'];
+  displayedColumns2: string[] = ['countryName', 'detail'];
+  allCount: any;
+  allList: any = new MatTableDataSource();
+  allCountry: any;
+  allCountryCount: any;
 
+  constructor(
+    private dashboardService: DashboardService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.getAllCount();
+    this.getAllList();
+    this.getAllCountry();
+  }
+
+  // 모든 개수 조회
+  getAllCount() {
+    this.dashboardService.getAllCount().subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.allCount = res.data;
+      },
+      error: (err: any) => {
+        console.error(err);
+      },
+    });
+  }
+
+  // 모든 리스트 조회
+  getAllList() {
+    this.dashboardService.getAllList().subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.allList = new MatTableDataSource<PeriodicElement>(res.allList);
+        this.allList.paginator = this.paginator;
+      },
+      error: (err: any) => {
+        console.error(err);
+      },
+    });
+  }
+
+  // 모든 개수 조회
+  getAllCountry() {
+    this.dashboardService.getAllCountry().subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.allCountry = res.data;
+        this.allCountryCount = res.count;
+      },
+      error: (err: any) => {
+        console.error(err);
+      },
+    });
+  }
+
+  selectHoliday(countryId: any) {
+    console.log(countryId);
+    this.router.navigate(['/country/' + countryId]);
+  }
 }
---
+++ src/app/services/dashboard.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DashboardService {
+  private baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  //모든 개수 목록 조회
+  getAllCount() {
+    return this.http.get(this.baseUrl + '/dashboard');
+  }
+
+  //모든 리스트 목록 조회
+  getAllList() {
+    return this.http.get(this.baseUrl + '/dashboard/list');
+  }
+
+  //모든 나라 목록 조회
+  getAllCountry() {
+    return this.http.get(this.baseUrl + '/dashboard/country');
+  }
+}
